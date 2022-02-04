4c4b21fd39f1f248cbc5b154df515d991011b052
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-02-04 14:36:44 -0700

    Update game stats method

--- game.rb
+++ game.rb
@@ -66,6 +66,11 @@ class Game
             puts "#{formatted_name} #{player.score}"
         end
 
+        @players.each do |player|
+            puts "\n#{player.name}'s point totals:"
+            puts "#{player.points} grand total points"
+        end
+
     end
 
 end
